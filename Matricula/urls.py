94336a07e512121e4c69f481af19bacfb4073b54
KevinSucapuca <ksucapuca007@gmail.com>
2023-07-13 18:26:26 -0500

    add urls for docente

--- Matricula/urls.py
+++ Matricula/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home,name='home'),
-    
+    path('admin-docente/', views.Docente,name='admin-docente'),
+    path('lista-docente/', views.ListaDocente,name='lista-docente'),
+    path('buscar-docente/', views.BuscarDocente,name='buscar-docente'),
 ]
