bdad3333038ef3491643fb71121a5d54a4420564
KevinSucapuca <ksucapuca007@gmail.com>
2023-07-14 00:46:43 -0500

    add url for curso

--- Matricula/urls.py
+++ Matricula/urls.py
@@ -6,4 +6,11 @@ urlpatterns = [
     path('admin-docente/', views.Docente,name='admin-docente'),
     path('lista-docente/', views.ListaDocente,name='lista-docente'),
     path('buscar-docente/', views.BuscarDocente,name='buscar-docente'),
+    path('admin-alumno/', views.Alumno,name='admin-alumno'),
+    path('lista-alumno/', views.ListaAlumno,name='lista-alumno'),
+    path('buscar-alumno/', views.BuscarAlumno,name='buscar-alumno'),
+    path('admin-curso/', views.Curso,name='admin-curso'),
+    path('lista-curso/', views.ListaCurso,name='lista-curso'),
+    path('buscar-curso/', views.BuscarCurso,name='buscar-curso'),
+    
 ]
