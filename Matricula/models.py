c9b22a867eb76d739e4c5dd16035d30811bbf3a5
KevinSucapuca <ksucapuca007@gmail.com>
2023-07-19 21:38:23 -0500

    add model Alumno

--- Matricula/models.py
+++ Matricula/models.py
@@ -13,25 +13,14 @@ class Docente(models.Model):
         data = "{0} {1} {2}"
         return data.format(self.dni,self.nombre, self.apellido)
         
+
+class Alumno(models.Model):
+    dni = models.CharField(max_length=8)
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    telefono = models.CharField(max_length=13)
+    direccion = models.CharField(max_length=300)
     
-
-Alumnos
-dni
-nombres
-apellidos
-telefono
-direccion
-
-Curso
-nombre curso
-vacantes
-docente
-
-
-Ciclo
-numero ciclo
-carrera
-
-CicloCurso
-ciclo
-curso
+    def __str__(self):
+        data = "{0} {1} {2}"
+        return data.format(self.dni,self.nombre, self.apellido)   
