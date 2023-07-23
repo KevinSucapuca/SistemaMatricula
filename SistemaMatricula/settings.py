c864cf79298dc89cb388a52456bfef4ab317ce42
KevinSucapuca <ksucapuca007@gmail.com>
2023-07-22 23:42:59 -0500

    change settings

--- SistemaMatricula/settings.py
+++ SistemaMatricula/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -27,6 +29,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+MESSAGE_STORAGE = "django.contrib.messages.storage.cookie.CookieStorage"
 
 # Application definition
 
